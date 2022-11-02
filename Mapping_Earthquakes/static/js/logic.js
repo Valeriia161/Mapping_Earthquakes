c74bc36e0e21149ace7d7baf6773bbf252e58d0c
Valeriia161 <leraozdoeva@gmail.com>
2022-11-02 11:54:44 -0400

    updating

--- Mapping_Earthquakes/static/js/logic.js
+++ Mapping_Earthquakes/static/js/logic.js
@@ -1,5 +1,3 @@
-// Add boilerplate code 
-
 // Add console.log to check to see if our code is working.
 console.log("working");
 
@@ -8,25 +6,27 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 
+// // An alternative to using the setView() method:
+// let map = L.map("mapid", {
+//     center: [
+//       40.7, -94.5
+//     ],
+//     zoom: 4
+//   });
+// // This method is useful when we need to add multiple tile layers, or a background image of our map(s).
 
-// Get data from cities.js
-let cityData = cities;
-
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function (city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population / 200000,
-        color: "orange",
-        fillColor: "#ffffa1",
-        weight: 4
-    })
-        .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() // format the population with a thousands separator by using the toLocaleString() method on the city.population in the bindPopup() method
-            + "</h3>")
-        .addTo(map);
-});
 
 // Tile Layer (using to load and display a tile layer on the map):
+// We create the tile layer that will be the background of our map.
+//  Add a marker to the map for Los Angeles, California.
+
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+let marker = L.circleMarker([34.0522, -118.2437], {
+    radius: 10,
+    color: "black",
+    fillColor: "#ffffa1"
+}).addTo(map);
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
